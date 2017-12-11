e7b53d97700f7948bc4a067b340a2d060c424f0e
Alesia Kolbik <moyaalesia@mail.ru>
2017-12-11 16:44:34 +0300

    refactor the code

--- src/com/testpackage/Main.java
+++ src/com/testpackage/Main.java
@@ -7,40 +7,49 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sca = new Scanner(System.in);
-        System.out.println("Введите предложение:");
+        System.out.println("Введите строку слов, разделенных пробелами:");
         String stringOfSymbols = sca.nextLine();
         Scanner sca2 = new Scanner(System.in);
         System.out.println("Введите слово:");
         String word = sca2.nextLine();
+        word.trim();
         ArrayList<String> arrayOfWords = new ArrayList<>(1000);
-        transformStringToArray(stringOfSymbols,arrayOfWords);
-        for (int i = 0; i <= arrayOfWords.size(); i++) {
-            int lengthWordInArray = arrayOfWords.get(i).length();
-            if (word.length() == lengthWordInArray) {
+        fillToArray(stringOfSymbols, arrayOfWords);
+        int numberOfMismatches=0;
+        for (int i = 0; i < arrayOfWords.size(); i++) {
+            int lengthWordInArray = arrayOfWords.get(i).length();  //длинна слова в массиве
+            int lengthWord=word.length();    //длинна заданного слова
+            if (lengthWord == lengthWordInArray) {
                 System.out.println(arrayOfWords.get(i));
-            } else {
+            }
+            else {
+                numberOfMismatches++;
+            }
+            if(numberOfMismatches==arrayOfWords.size())
                 System.out.println("Совпадений со словом " + word + " не найдено.");
-            }
         }
 
 
     }
 
-    public static void transformStringToArray(String string, ArrayList<String> resultArray) {
+    public static void fillToArray(String string, ArrayList<String> resultArray) {
         String word = "";
-        int index=0;
+        int indexArray = 0;
         for (int i = 0; i < string.length(); i++) {
-            boolean isContainsSpace=String.valueOf(' ').contains(string.charAt(i));
-            if (isContainsSpace ==false){
+            boolean isSpace = String.valueOf(' ').contains(String.valueOf(string.charAt(i)));
+            if (!isSpace) {
+                word+=string.charAt(i);
                 if (i == (string.length() - 1)) {
-                    resultArray.add(index,word);
+                    resultArray.add(indexArray, word);
                 }
-            } else {
-                if (!(string.charAt(0) == ' ' ))
-                    resultArray.add(index,word);
+            }
+            else {
+                if ((string.charAt(0) != ' ') ||( string.charAt(i - 1) != ' ')) {
+                    resultArray.add(indexArray, word);
                     word = "";
-                    index++;
+                    indexArray++;
                 }
             }
         }
     }
+}
